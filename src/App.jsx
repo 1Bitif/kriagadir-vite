fc876a1d0584c45f4ebed11fecb3367e8cd77458
1Bitif <aajliabdellatif@gmail.com>
2026-06-06 01:05:45 +0100

    repaired app

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,44 @@
-import './App.css'
-import Hero from './components/pages/Hero'
-import Navbar from './components/pages/Navbar'
-
+import { useState } from "react";
+import "./App.css";
+import BrandSlider from "./components/pages/BrandSlider";
+import FleetSection from "./components/pages/FleetSection";
+import Hero from "./components/pages/Hero";
+import HowItWorks from "./components/pages/HowItWorks";
+import Navbar from "./components/pages/Navbar";
+import BookingModal from "./components/pages/BookingModal";
+import ContactSection from "./components/pages/ContactSection";
+import { Toaster } from "sonner";
+import WhatsAppWidget from "./components/pages/WhatsAppWidget";
+import Footer from "./components/pages/Footer";
 
 function App() {
+  const [selectedCar, setSelectedCar] = useState(null);
+  const [showModal, setShowModal] = useState(false);
+  const [showChatWidget, setShowChatWidget] = useState(false); 
+
   return (
     <div className="App">
       <Navbar />
-     <Hero />
-       {/* <BrandSlider />
+      <Hero />
+      <BrandSlider />
       <HowItWorks />
-      <FleetSection setSelectedCar={setSelectedCar} />
+      <FleetSection
+        setSelectedCar={setSelectedCar}
+        setShowModal={setShowModal}
+      />
+      <BookingModal 
+        selectedCar={selectedCar}
+        setSelectedCar={setSelectedCar}
+        showModal={showModal}
+        setShowModal={setShowModal}
+      />
       <ContactSection />
-      <WhatsAppWidget showChatWidget={showChatWidget} setShowChatWidget={setShowChatWidget} />
-      <BookingModal selectedCar={selectedCar} setSelectedCar={setSelectedCar} />
-      <Footer /> */}
+      <WhatsAppWidget 
+        showChatWidget={showChatWidget} 
+        setShowChatWidget={setShowChatWidget} 
+      />
+      <Footer />
+      <Toaster richColors position="top-right" />
     </div>
   );
 }
